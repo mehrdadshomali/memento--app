285f28a1d62d0c28b872eb34529934f3fb6345f3
mehrdad shomali <mehrdadshomali99@gmail.com>
2026-01-23 01:02:42 +0300

    Remove AuthScreen and AuthContext files

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,3 @@
-export { default as AuthScreen } from './AuthScreen';
 export { HomeScreen } from './HomeScreen';
 export { GameScreen } from './GameScreen';
 export { ProfileSelectScreen } from './ProfileSelectScreen';
